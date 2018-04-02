2e0934089554b18ca8ee2e2285224556deb5cf1f
Rafael B.C <bcrafa@gmail.com>
2018-04-02 14:09:57 +0200

    Complex dashboards

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ echo "Waiting for 10 seconds..."
 sleep 10
 
 #Configure Grafana
-curl -vvvv --user admin:admin -X POST -H 'Content-Type: application/json' --data @config/dataSource.json http://localhost:3000/api/datasources
-curl -vvvv --user admin:admin -X POST -H 'Content-Type: application/json' --data @config/dashboard.json http://localhost:3000/api/dashboards/db
-export DASHBOARD_ID=$(curl --user admin:admin http://localhost:3000/api/dashboards/db/sample-dashboard 2>/dev/null|jq .dashboard.id)
-echo "{\\"homeDashboardId\\":${DASHBOARD_ID}}" | curl -s --show-error --user admin:admin -X PUT -H 'Content-Type: application/json' --data @- http://localhost:3000/api/user/preferences
+curl -vvvv --user admin:admin -H 'Content-Type: application/json' --data @config/dataSource.json http://localhost:3000/api/datasources
+curl -vvvv --user admin:admin -H 'Content-Type: application/json' --data @config/server_dashboard.json http://localhost:3000/api/dashboards/db
+curl -vvvv --user admin:admin -H 'Content-Type: application/json' --data @config/people_dashboard.json http://localhost:3000/api/dashboards/db
+curl -vvvv --user admin:admin -H 'Content-Type: application/json' --data @config/actuator_dashboard.json http://localhost:3000/api/dashboards/db
